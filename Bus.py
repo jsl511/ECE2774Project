00092e310dc2c6ba737bf3299286e286bafc0981
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-02-19 16:01:26 -0500

    Implement bus counting as class variable

--- Bus.py
+++ Bus.py
@@ -1,13 +1,7 @@
 class Bus:
-    busCount = 0
+    bus_count = 0
 
     def __init__(self, name):
         self.name = name
-        self.index = Bus.busCount
 
-        self.voltage = None
-
-        Bus.busCount = Bus.busCount + 1
-
-    def setVoltage(self, voltage):
-        self.voltage = voltage
+        Bus.bus_count += 1
