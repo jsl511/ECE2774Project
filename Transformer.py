9ac6bb685d3784598cfea2952ceacdb939a288fb
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-02-19 15:57:49 -0500

    Calculate impedance in a function

--- Transformer.py
+++ Transformer.py
@@ -1,17 +1,18 @@
 import math
-from Bus import Bus
 
 
 class Transformer:
-    def __init__(self, name, powerRating, voltageRatingHigh, voltageRatingLow, impedance, xR, bus1Connection, bus2Connection):
+    def __init__(self, name, power, voltage1, voltage2, impedance, x_r, bus1, bus2):
         self.name = name
-        self.powerRating = powerRating
-        self.voltageRatingHigh = voltageRatingHigh
-        self.voltageRatingLow = voltageRatingLow
-        self.impedance = impedance  # per-unit impedance
-        self.xR = xR
-        self.bus1Connection = Bus(bus1Connection)
-        self.bus2Connection = Bus(bus2Connection)
+        self.power = power          # rated power (MVA)
+        self.voltage1 = voltage1    # high-side voltage rating (kV)
+        self.voltage2 = voltage2    # low-side voltage rating (kV)
+        self.impedance = impedance  # rated impedance (pu)
+        self.x_r = x_r              # X/R ratio
+        self.bus1 = bus1            # high-side bus connection
+        self.bus2 = bus2            # low-side bus connection
 
-        self.Impedance = self.impedance*math.e**(1j*math.atan(self.xR))
-        self.Admittance = 1/self.Impedance
+        self.transformer_impedance = None
+
+    def calculate_impedance(self):
+        self.transformer_impedance = self.impedance*math.e**(1j*math.atan(self.x_r))
