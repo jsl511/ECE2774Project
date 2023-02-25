1f9a0b566fb5b99fb5cf318ef449d6014a405eaa
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-02-25 14:17:56 -0500

    Construct system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from System import System
+from Ybus import Ybus
 
 # create the 6-node looped transmission system
 system = System()
@@ -11,4 +12,13 @@ system.geometry.calculate_GMD()
 
 system.add_transformer("T1", 125, 20, 230, 0.085, 10, "1", "2")
 system.add_transformer("T2", 200, 20, 230, 0.105, 12, "6", "7")
+
 system.add_line("L1", 10, "2", "4", system.bundle, system.geometry)
+system.add_line("L2", 25, "2", "3", system.bundle, system.geometry)
+system.add_line("L3", 20, "3", "6", system.bundle, system.geometry)
+system.add_line("L4", 20, "4", "6", system.bundle, system.geometry)
+system.add_line("L5", 10, "6", "5", system.bundle, system.geometry)
+system.add_line("L6", 35, "4", "5", system.bundle, system.geometry)
+
+y_bus = Ybus()
+y_bus.calculate_Ybus(system)
