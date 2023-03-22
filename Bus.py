abc34f4709a91d64fdb43267b207007202f47ecd
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-03-22 13:12:27 -0400

    Switch order of angle and voltage in set_voltage()

--- Bus.py
+++ Bus.py
@@ -11,10 +11,13 @@ class Bus:
 
         Bus.bus_count += 1
 
-    def set_voltage(self, voltage, angle):
-        self.voltage = voltage
+    def set_voltage(self, angle, voltage):
         self.angle = angle
+        self.voltage = voltage
 
     def set_power(self, power, reactive):
         self.power = power
         self.reactive = reactive
+
+    def get_power(self):
+        return self.power
