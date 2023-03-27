853efbcec7ef8f224fbab5d32dfbd66819b4cb65
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-03-27 15:47:26 -0400

    Implement power mismatch calculation

--- PowerFlow.py
+++ PowerFlow.py
@@ -1,18 +1,21 @@
 import numpy as np
+import math
+np.set_printoptions(threshold=np.inf)
 
 
 class PowerFlow:
     def __init__(self, system, y_bus, slack_bus):
         self.system = system
-        self.ybus = y_bus
+        self.Y_bus = np.absolute(y_bus)
+        self.theta = np.angle(y_bus)
         self.N = len(self.system.buses)     # number of buses (includes the slack bus)
 
         # voltage and power vectors for power flow
-        self.x = np.empty(2 * self.N)
-        self.y = np.empty(2 * self.N)
+        self.x = np.ones(2 * self.N)
+        self.y = np.ones(2 * self.N)
 
         # initialize y vector with bus powers
-        for k in range(self.N):         # for k from 0 to 6 (for loops count up to, not including)
+        for k in range(self.N):         # for k from 0 to 7 (for loops count up to, not including)
             if (k + 1) == slack_bus:    # the slack bus has no power ratings (?)
                 self.y[k] = None
                 self.y[k + self.N] = None
@@ -29,5 +32,18 @@ class PowerFlow:
             self.system.buses.get(str(k + 1)).set_voltage(self.x[k], self.x[k + self.N])
 
     def solve_newton_raphson(self):
-        # step 1: compute the updated y vector using start data
-        pass
+        # step 1: update the y vector using start data
+        for k in range(self.N):
+            P, Q = 0, 0
+
+            for n in range(self.N):
+                P += self.Y_bus[k,n] * self.x[n + self.N] * math.cos(self.x[k] - self.x[n] - self.theta[k,n])
+                Q += self.Y_bus[k,n] * self.x[n + self.N] * math.sin(self.x[k] - self.x[n] - self.theta[k,n])
+
+            # print(cos)
+            # print(sin)
+            # print("k = " + str(k) + "\n")
+            self.y[k] -= self.x[k + self.N] * P
+            self.y[k + self.N] -= self.x[k + self.N] * Q
+
+        print(np.around(self.y, decimals=2))
