cfb09ed2908d06b3ced94824500ffa1c29a1f2b8
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-03-27 16:26:02 -0400

    Rearrange parameter calculations and conversions

--- TransmissionLine.py
+++ TransmissionLine.py
@@ -12,12 +12,9 @@ class TransmissionLine:
         self.geometry = geometry    # Geometry object
 
         self.capacitance = (2*math.pi*8.854*10**(-12))/math.log(self.geometry.D_eq/self.bundle.D_SC)    # (F/m)
-        self.capacitance = self.capacitance/0.000621371     # convert from (F/m) to (F/mi)
+        self.inductance = (2*10**(-7))*math.log(self.geometry.D_eq/self.bundle.D_SL)                    # (H/m)
+        self.impedance = (self.bundle.resistance*self.length) + 1j*377*(self.inductance*self.length)    # series impedance (Ohm)
+        self.admittance = 1j*377*(self.capacitance*self.length)                                         # shunt admittance (S)
 
-        self.inductance = (2*10**(-7))*math.log(self.geometry.D_eq/self.bundle.D_SL)    # (H/m)
-        self.inductance = self.inductance/0.000621371                                   # convert from (H/m) to (H/mi)
-
-        # series impedance of the line (Ohms)
-        self.impedance = (self.bundle.resistance*self.length) + 1j*377*(self.inductance*self.length)
-
-        self.admittance = 1j*377*(self.capacitance*self.length)     # shunt admittance of the line (S)
+        self.capacitance = self.capacitance / 0.000621371   # convert from (F/m) to (F/mi)
+        self.inductance = self.inductance / 0.000621371     # convert from (H/m) to (H/mi)
