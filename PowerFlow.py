00b815a04a09c27be14ecac86a4adbb256b99e6f
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-04-01 14:01:05 -0400

    Assign bus power and populate x from those

--- PowerFlow.py
+++ PowerFlow.py
@@ -13,6 +13,12 @@ class PowerFlow:
         self.x = np.ones(2 * self.N)
         self.y = np.ones(2 * self.N)
 
+        # create empty jacobian quadrants
+        self.jacobian1 = None
+        self.jacobian2 = None
+        self.jacobian3 = None
+        self.jacobian4 = None
+
         # initialize y vector with bus powers
         for k in range(self.N):         # for k from 0 to 7 (for loops count up to, not including)
             if (k + 1) == slack_bus:    # the slack bus has no power ratings (?)
@@ -22,19 +28,11 @@ class PowerFlow:
                 self.y[k] = self.system.buses.get(str(k + 1)).power
                 self.y[k + self.N] = self.system.buses.get(str(k + 1)).reactive
 
-        # create empty jacobian quadrants
-        self.jacobian1 = None
-        self.jacobian2 = None
-        self.jacobian3 = None
-        self.jacobian4 = None
-
     def flat_start(self):
         for k in range(self.N):
-            self.x[k] = 0
-            self.x[k + self.N] = 1.0
-
-            # initialize bus object voltages using x data
-            self.system.buses.get(str(k + 1)).set_voltage(self.x[k], self.x[k + self.N])
+            self.system.buses.get(str(k + 1)).set_voltage(0, 1.0)
+            self.x[k] = self.system.buses.get(str(k + 1)).angle
+            self.x[k + self.N] = self.system.buses.get(str(k + 1)).voltage
 
     def solve_newton_raphson(self):
         # step 1: update the y vector using start data
@@ -45,10 +43,11 @@ class PowerFlow:
                 P += self.Y_bus[k,n] * self.x[n + self.N] * math.cos(self.x[k] - self.x[n] - self.theta[k,n])
                 Q += self.Y_bus[k,n] * self.x[n + self.N] * math.sin(self.x[k] - self.x[n] - self.theta[k,n])
 
-            # print(cos)
-            # print(sin)
-            # print("k = " + str(k) + "\n")
             self.y[k] -= self.x[k + self.N] * P
             self.y[k + self.N] -= self.x[k + self.N] * Q
 
+        np.set_printoptions(linewidth=200)
         print(np.around(self.y, decimals=2))
+
+    def solve_jacobian(self):
+        pass
