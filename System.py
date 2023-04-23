fb65ffe071db0f60280f529069f5f4816c1906cb
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-04-23 11:18:00 -0400

    Calculate voltage bases and other bases
    This functionality exists but is not implemented everywhere it should be

--- System.py
+++ System.py
@@ -7,7 +7,7 @@ from Transformer import Transformer
 
 
 class System:
-    def __init__(self):
+    def __init__(self, S_base):
         # assumes one conductor, geometry, and bundling configuration for the entire system
         # to accommodate multiples, change to str:Obj dictionaries; methods to add_<element>()
         self.conductor = None
@@ -18,6 +18,11 @@ class System:
         self.buses = {}
         self.lines = {}
         self.transformers = {}
+
+        # MVA base for entire system and empty lists to hold V and Z bases for different regions
+        self.S_base = S_base
+        self.V_bases = []
+        self.Z_bases = []
 
     def __add_bus(self, name):
         if name not in self.buses.keys():
@@ -35,6 +40,12 @@ class System:
             self.__add_bus(bus1)
             self.__add_bus(bus2)
 
+            if voltage1 not in self.V_bases:
+                self.__add_base(voltage1)
+
+            if voltage2 not in self.V_bases:
+                self.__add_base(voltage2)
+
     def set_conductor(self, name, diameter, GMR, ampacity, resistance):
         self.conductor = Conductor(name, diameter, GMR, ampacity, resistance)
 
@@ -43,3 +54,12 @@ class System:
 
     def set_geometry(self, x_a, x_b, x_c, y_a, y_b, y_c):
         self.geometry = Geometry(x_a, x_b, x_c, y_a, y_b, y_c)
+
+    def __add_base(self, V_base):
+        self.V_bases.append(V_base)
+        # I_base = self.S_base/V_base     # check for LL or LG voltage
+        Z_base = (V_base * V_base)/self.S_base  # assumes LL
+        self.Z_bases.append(Z_base)
+
+        # regions are determined by index of lists
+            # how to track current region? by bus?
