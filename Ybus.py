e8c2615579c22d5bb83b8f33eee28b2cf5092283
Graymalk1n <77937034+Graymalk1n@users.noreply.github.com>
2023-04-26 12:46:51 -0400

    Calculate off-diagonal elements correctly
    Previously, the admittance between buses was calculated to include the shunt admittance of the line. This was incorrect, since the shunt admittance is between a bus and ground. Now the admittance between buses includes only the inverse of the series impedance of the line. The resulting Ybus matrix matches what PowerWorld calculates.

--- Ybus.py
+++ Ybus.py
@@ -8,27 +8,28 @@ class Ybus:
         self.y_bus = np.zeros((len(self.system.buses), len(self.system.buses)), dtype=complex)
 
         # transformer admittance
-        for value in self.system.transformers.values():
-            bus1 = int(value.bus1) - 1
-            bus2 = int(value.bus2) - 1
+        for transformer in self.system.transformers.values():
+            bus1 = int(transformer.bus1) - 1
+            bus2 = int(transformer.bus2) - 1
 
-            self.y_bus[bus1, bus2] -= [value.admittance]
-            self.y_bus[bus1, bus1] += [value.admittance]
-            self.y_bus[bus2, bus2] += [value.admittance]
-            self.y_bus[bus2, bus1] = self.y_bus[bus1, bus2]
+            self.y_bus[bus1,bus2] -= [transformer.admittance]
+            self.y_bus[bus1,bus1] += [transformer.admittance]
+            self.y_bus[bus2,bus2] += [transformer.admittance]
+            self.y_bus[bus2,bus1] = self.y_bus[bus1,bus2]
 
         # transmission line admittance
-        for value in self.system.lines.values():
-            bus1 = int(value.bus1) - 1
-            bus2 = int(value.bus2) - 1
+        for line in self.system.lines.values():
+            bus1 = int(line.bus1) - 1
+            bus2 = int(line.bus2) - 1
 
-            self.y_bus[bus1, bus2] -= [1/value.impedance + value.admittance]
-            self.y_bus[bus1, bus1] += [1/value.impedance + value.admittance/2]
-            self.y_bus[bus2, bus2] += [1/value.impedance + value.admittance/2]
-            self.y_bus[bus2, bus1] = self.y_bus[bus1, bus2]
+            self.y_bus[bus1,bus2] -= [1/line.impedance]
+            self.y_bus[bus1,bus1] += [1/line.impedance + line.admittance/2]
+            self.y_bus[bus2,bus2] += [1/line.impedance + line.admittance/2]
+            self.y_bus[bus2,bus1] = self.y_bus[bus1,bus2]
 
         # np.set_printoptions(linewidth=200)
-        # print("y_bus is: \n" + str(np.around(self.y_bus, decimals=2)))
-        # np.savetxt("ybus.csv", self.y_bus, delimiter=",")
+        # print("y_bus is: \n" + str(self.y_bus))
+        # print()
+        # print("y_bus rounded is: \n " + str(np.around(self.y_bus, decimals=2)))
         # print(np.around(np.absolute(self.y_bus), decimals=2))
         # print(np.around(np.angle(self.y_bus), decimals=2))
